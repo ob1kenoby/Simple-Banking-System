8d94da65df2d75c94c9173e03c8ed2bc999edd4d
Sergey Mikhaylov <bodmode@gmail.com>
2020-10-17 06:13:56 +0300

    implement login function

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,11 +1,13 @@
 from random import randint
 
 
+card_data = {}
+
+
 def create_account():
-    card_number = "".join([randint(0, 9) for _i in range(0, 10)])
-    pin = "".join([randint(0, 9) for _i in range(0, 4)])
+    card_number = "400000" + "".join([str(randint(0, 9)) for _i in range(0, 10)])
+    pin = "".join([str(randint(0, 9)) for _i in range(0, 4)])
     card_data[card_number] = pin
-    print()
     print("Your card has been created")
     print("Your card number:")
     print(card_number)
@@ -14,22 +16,26 @@ def create_account():
 
 
 def login():
-    pass
+    card_number = input("Enter your card number: ")
+    pin = input("Enter your PIN: ")
+    print()
+    if card_number not in card_data:
+        print("Wrong card number or PIN!")
+        if card_data[card_number] != pin:
+            print("Wrong card number or PIN!")
+    elif card_data[card_number] == pin:
+        print("You have successfully logged in!")
 
 
-card_data = {}
-
-print("1. Create an account")
-print("2. Log into account")
-print("0. Exit")
-
-exit_choice = False
-
-while not exit_choice:
+user_choice = ""
+while not user_choice == "0":
+    print("1. Create an account")
+    print("2. Log into account")
+    print("0. Exit")
+    print()
     user_choice = input()
     if user_choice == "1":
         create_account()
     elif user_choice == "2":
         login()
-    elif user_choice == "0":
-        exit_choice = True
+    print()
